01e09d46d351915d6f5a556b905a20ee8f997985
Dipper GUO <c-guojian@genomics.cn>
2019-08-29 20:19:23 +0800

    Add: i18n
    1. modified navbar.js
    2. add i18n motiple language

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,39 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import React from "react";
+import { Link } from "gatsby";
+import logo from "../img/logo.png";
+import { withTranslation } from "react-i18next";
 
-const Navbar = class extends React.Component {
+class Navbar extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       active: false,
-      navBarActiveClass: '',
-    }
+      navBarActiveClass: ""
+    };
   }
 
   toggleHamburger = () => {
     // toggle the active boolean in the state
     this.setState(
       {
-        active: !this.state.active,
+        active: !this.state.active
       },
       // after state has been updated,
       () => {
         // set the class in state for the navbar accordingly
         this.state.active
           ? this.setState({
-              navBarActiveClass: 'is-active',
+              navBarActiveClass: "is-active"
             })
           : this.setState({
-              navBarActiveClass: '',
-            })
+              navBarActiveClass: ""
+            });
       }
-    )
-  }
+    );
+  };
 
   render() {
+    const { t } = this.props;
     return (
       <nav
         className="navbar is-transparent"
@@ -42,7 +43,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={logo} alt="Haetex" style={{ width: "88px" }} />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -61,19 +62,19 @@ const Navbar = class extends React.Component {
           >
             <div className="navbar-start has-text-centered">
               <Link className="navbar-item" to="/about">
-                About
+                {t("about")}
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
+                {t("products")}
               </Link>
               <Link className="navbar-item" to="/blog">
-                Blog
+                {t("blog")}
               </Link>
               <Link className="navbar-item" to="/contact">
-                Contact
+                {t("contact")}
               </Link>
               <Link className="navbar-item" to="/contact/examples">
-                Form Examples
+                {t("example")}
               </Link>
             </div>
             <div className="navbar-end has-text-centered">
@@ -83,16 +84,14 @@ const Navbar = class extends React.Component {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
+                <span className="icon">{t("login")}</span>
               </a>
             </div>
           </div>
         </div>
       </nav>
-    )
+    );
   }
 }
 
-export default Navbar
+export default withTranslation("navbar")(Navbar);
