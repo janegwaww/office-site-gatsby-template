4ed2e74db30476e43c850df817e44884ea9d1e9b
Dipper GUO <c-guojian@genomics.cn>
2019-08-30 12:53:41 +0800

    revert

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,6 @@
 var proxy = require('http-proxy-middleware')
 
 module.exports = {
-  pathPrefix: `/gatsby-starter-netlify-cms`,
   siteMetadata: {
     title: 'Gatsby + Netlify CMS Starter',
     description:
