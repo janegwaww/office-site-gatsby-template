5852365bd9e542afb98a130271ec044bc354030a
Dipper GUO <c-guojian@genomics.cn>
2019-09-06 19:47:56 +0800

    Update: index page

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -25,7 +25,52 @@ export const IndexPageTemplate = ({
         backgroundAttachment: `fixed`,
         backgroundColor: `gray`
       }}
-    ></div>
+    >
+      <div
+        style={{
+          display: "flex",
+          height: "150px",
+          lineHeight: "1",
+          justifyContent: "space-around",
+          alignItems: "left",
+          flexDirection: "column"
+        }}
+      >
+        <h1
+          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
+          style={{
+            /* boxShadow: */
+            /*   "rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px", */
+            /* backgroundColor: "rgb(255, 68, 0)", */
+            color: "white",
+            lineHeight: "1",
+            padding: "0.25em"
+          }}
+        >
+          {`数据场景化搭建`}
+        </h1>
+        <h3
+          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
+          style={{
+            /* boxShadow: */
+            /*   "rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px", */
+            /* backgroundColor: "rgb(255, 68, 0)", */
+            color: "white",
+            lineHeight: "1",
+            padding: "0.25em"
+          }}
+        >
+          {`实现数据的按需汇聚，让应用像阳光，照亮业务场景的每个角落`}
+        </h3>
+        <div
+          style={{
+            padding: "0.25em"
+          }}
+        >
+          <a className="button is-white is-normal">了解更多</a>
+        </div>
+      </div>
+    </div>
     <section className="section section--gradient">
       <div className="container">
         <div className="section">
