c1875194f4d9617c7183ba01d2586154f0ea187c
Dipper GUO <c-guojian@genomics.cn>
2019-09-09 14:01:15 +0800

    Modified: index and product center page

--- src/components/BusinessCase.js
+++ src/components/BusinessCase.js
@@ -3,31 +3,42 @@ import PropTypes from "prop-types";
 import PreviewCompatibleImage from "../components/PreviewCompatibleImage";
 
 function BusinessCase() {
-  const caseObj = [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2];
+  const caseObj = [1, 2, 3, 4, 5, 6];
   return (
     <div>
       <div className="columns">
         <div className="column is-5">
-          <h4 className="has-text-weight-bold is-size-5">爱奇艺</h4>
+          <h4 className="has-text-weight-bold is-size-5">中国科学院</h4>
           <p>
-            爱奇艺是国内领先的视频内容平台，同时也是国内付费用户规模最大的平台之一。百度智能云支撑爱奇艺IT巨大的计算、存储、流量需求，帮助爱奇艺从一次性大笔投入重资产，变为分批租赁轻资产模式，加快交付速度，优化成本控制
+            中国科学院（英文名称：Chinese Academy of
+            Sciences，简称中科院）成立于1949年11月，为中国自然科学最高学术机构、科学技术最高咨询机构、自然科学与高技术综合研究发展中心。
           </p>
           <br />
           <p>
-            <span className="has-text-weight-bold">降低成本</span>:
-            改自建为租赁，通过系统算法动态选择更便宜的CDN
+            <img src={`./img/check.png`} />
+            <span className="has-text-weight-bold">打造智能知识库：</span>:
+            提供标准知识库外，持续挖掘用户与机器人客服的历史会话进 行自主学习
           </p>
-          {/* <p>降低成本: 改自建为租赁，通过系统算法动态选择更便宜的CDN</p> */}
-          {/* <p>降低成本: 改自建为租赁，通过系统算法动态选择更便宜的CDN</p> */}
+          <br />
+          <p>
+            <img src={`./img/check.png`} />
+            <span className="has-text-weight-bold">体验提升:</span>:
+            选取CDN体验更好，用户感受得到提升
+          </p>
           <br />
           <p>相关产品</p>
+          <div>
+            <span style={{ marginRight: "20px" }}>自然语言学习</span>
+            <span style={{ marginRight: "20px" }}>机器学习</span>
+            <span style={{ marginRight: "20px" }}>深度学习</span>
+          </div>
         </div>
         <div className="column is-7">
           <div className="columns is-multiline is-variable is-3">
             {caseObj.map((o, i) => (
               <div
                 className="column is-4"
-                style={{ boxShadow: "0px 4px 10px 0px rgba(0,0,0,0.1)" }}
+                /* style={{ boxShadow: "0px 4px 10px 0px rgba(0,0,0,0.1)" }} */
               >
                 <PreviewCompatibleImage
                   imageInfo={{
--- src/templates/product-center-page.js
+++ src/templates/product-center-page.js
@@ -37,7 +37,7 @@ const ProductCenterTemplate = ({ itemInfo }) => (
                 /* backgroundColor: "rgb(255, 68, 0)", */
                 color: "white",
                 lineHeight: "1",
-                padding: "0.25em"
+                padding: "0.25em 0"
               }}
             >
               {`黑顿算法模块变量引擎 Seeker`}
@@ -50,24 +50,36 @@ const ProductCenterTemplate = ({ itemInfo }) => (
                 /* backgroundColor: "rgb(255, 68, 0)", */
                 color: "white",
                 lineHeight: "1",
-                padding: "0.25em"
+                padding: "0.5em 0em"
               }}
             >
               {`全面闭环的自动化机器学习平台。给定某业务场景，输入该场景各个变量的历史数据，变量引擎即能根据历史数据进行自主学习，挖掘出深层次的场景知识，形成变量之间的关联图谱。依托关联图谱，客户可对部分变量进行赋值，图谱便可对未被赋值的变量进行实时预测，实现渗透式的场景数据/变量深度挖掘。`}
             </h6>
-            <div className="columns" style={{ padding: "0.25em" }}>
-              <div className="column is-4">
-                <a className="button" href="#">
+            <div className="columns is-gapless" style={{ padding: "0.25em 0" }}>
+              <div className="column is-3">
+                <a className="button is-link" href="#">
                   立即使用
                 </a>
               </div>
-              <div className="column is-4">
-                <a className="button" href="#">
+              <div className="column is-3 has-text-left">
+                <a
+                  className="button"
+                  href="#"
+                  style={{ backgroundColor: "transparent", color: "white" }}
+                >
                   业务方案咨询
                 </a>
               </div>
-              <div className="column is-4">
-                <a className="has-text-white" href="#">
+              <div className="column is-6 has-text-centered">
+                <a
+                  className="button is-text "
+                  href="#"
+                  style={{
+                    backgroundColor: "transparent",
+                    color: "white",
+                    border: "unset"
+                  }}
+                >
                   帮助文档{">"}
                 </a>
               </div>
