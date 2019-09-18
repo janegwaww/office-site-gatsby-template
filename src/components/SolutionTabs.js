2db7217edcc4674577a93d4567f87942d4462888
Dipper GUO <c-guojian@genomics.cn>
2019-09-18 19:17:13 +0800

    Add: 解决方案模块添加幻灯片

--- src/components/SolutionTabs.js
+++ src/components/SolutionTabs.js
@@ -1,25 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import bulmaCarousel from "../../node_modules/bulma-extensions/bulma-carousel/dist/js/bulma-carousel.min.js";
 import ContainCard from "../components/ContainCard";
 import "./solution-tabs.sass";
 
 function SolutionTabs({ solutionItems }) {
-  let [items, setItems] = useState(
+  const [items, setItems] = useState(
     solutionItems.blurbs.map((o, i) =>
       i === 0 ? { ...o, className: "is-active" } : o
     )
   );
-  const _tabSelect = tab => {
+  const _tabSelect = key => {
     setItems(
-      items.map(o => {
-        if (o.heading === tab) {
-          return { ...o, className: "is-active" };
-        } else {
-          return { ...o, className: "" };
-        }
-      })
+      items.map((o, i) =>
+        i === key ? { ...o, className: "is-active" } : { ...o, className: "" }
+      )
     );
   };
+  useEffect(() => {
+    const carousels = bulmaCarousel.attach(`#carousel-solution`, {
+      initialSlide: 0,
+      slidesToScroll: 1,
+      slidesToShow: 1,
+      effect: "translate",
+      pagination: false
+    });
+  });
+
   return (
     <div className="solution-tabs">
       <h3 className="has-text-weight-bold is-size-3 has-text-white has-text-centered">
@@ -31,20 +38,19 @@ function SolutionTabs({ solutionItems }) {
           <div className="tabs is-centered is-fullwidth">
             <ul>
               {[...items].map((o, i) => (
-                <li key={i} data-tab={i} className={o.className}>
-                  <a onClick={() => _tabSelect(o.heading)}>{o.heading}</a>
+                <li key={i} className={o.className}>
+                  <a onClick={() => _tabSelect(i)}>{o.heading}</a>
                 </li>
               ))}
             </ul>
           </div>
         </div>
       </div>
-
       <br />
       <div className="container">
-        <div id="solu-tab-content">
+        <div id="carousel-solution" className="carousel">
           {[...items].map((o, i) => (
-            <div data-content={i} key={i} className={o.className}>
+            <div key={i} className={`item-${i}`} style={{ padding: "0 2rem" }}>
               <ContainCard info={{ ...o, button: "查看详情" }} />
             </div>
           ))}
