4b2d298881a916bed225c4559d89c20768958338
Dipper GUO <c-guojian@genomics.cn>
2019-09-25 09:18:59 +0800

    remove: “登录”

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -132,7 +132,6 @@ const Navbar = class extends React.Component {
 
             <div className="navbar-end has-text-centered">
               <div className="navbar-item is-expanded">
-                <a className="button is-white">登录</a>
                 <a className="button is-white">注册</a>
               </div>
               <div className="navbar-item">
