fdcc34fddb2ed087862064530ffaf2816e174d7c
Dipper GUO <tiglapiles@gmail.com>
2019-10-15 17:27:54 +0800

    fixed style: about us page
    1. banner image height
    2. content word line height and spacing
    3. content padding

--- src/templates/about-us-page.js
+++ src/templates/about-us-page.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 import PreviewCompatibleImage from "../components/PreviewCompatibleImage";
 import AboutFeatures from "../components/AboutFeatures";
+// import banner from "../img/aboutus1-mobile.png";
 
 const AboutUsTemplate = ({ image, intro, creaters }) => {
+  const [isMobile, setIsMobile] = useState(false);
   const companyIntro = intro.description.split(/\s{2}|\\/);
   const headStyle = (width = 280) => ({
     display: "flex",
@@ -13,14 +15,15 @@ const AboutUsTemplate = ({ image, intro, creaters }) => {
     alignItems: "center",
     maxWidth: `${width}px`,
     height: "30px",
-    margin: "auto"
-    // boxShadow:
-    // "rgba(12,102,255,1) 0.3rem 0px 0px, rgba(12,102,255,1) -0.3rem 0px 0px"
+    margin: "auto",
+    boxShadow: isMobile
+      ? "unset"
+      : "rgba(12,102,255,1) 0.3rem 0px 0px, rgba(12,102,255,1) -0.3rem 0px 0px"
   });
   const companyPara = para => {
     const pstyle = {
-      lineHeight: "26px",
-      letterSpacing: "1px",
+      lineHeight: isMobile ? "20px" : "26px",
+      letterSpacing: isMobile ? "0.04em" : "0.1em",
       padding: "1em 0em"
     };
     return para.map((o, i) => (
@@ -30,11 +33,18 @@ const AboutUsTemplate = ({ image, intro, creaters }) => {
     ));
   };
 
+  useEffect(() => {
+    if (window.innerWidth <= 768) {
+      setIsMobile(true);
+    }
+  }, []);
+
   return (
     <div className="about-us">
       <div
         className="full-width-image margin-top-0"
         style={{
+          height: isMobile ? "160px" : "500px",
           backgroundImage: `url(${
             !!image ? image.childImageSharp.fluid.src : image
           })`,
@@ -42,12 +52,12 @@ const AboutUsTemplate = ({ image, intro, creaters }) => {
           backgroundPosition: "center"
         }}
       />
-      <div className="section section--gradient">
+      <div className="about-us-content section section--gradient">
         <div className="container">
           <div className="columns">
             <div className="column is-10 is-offset-1">
               <div style={headStyle(540)}>
-                <h3 className="has-text-black is-size-3 has-text-centered is-size-5-5-mobile">
+                <h3 className="about-us-content-head has-text-black is-size-3 has-text-centered is-size-5-5-mobile">
                   {intro.heading}
                 </h3>
               </div>
