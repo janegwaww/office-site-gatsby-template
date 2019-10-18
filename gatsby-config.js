3ed9d96ce07ddea72c381502d2304555d77dada0
Dipper GUO <tiglapiles@gmail.com>
2019-10-18 21:53:20 +0800

    add baidu analytics and google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,6 +78,19 @@ module.exports = {
         purgeOnly: ["/all.sass"] // applies purging only on the bulma css file
       }
     }, // must be after other CSS plugins
+    {
+      resolve: `gatsby-plugin-baidu-analytics`,
+      options: {
+        siteId: "55635412905eb8d0d22476d497a731c0",
+        head: false
+      }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-150406661-1"
+      }
+    },
     "gatsby-plugin-netlify" // make sure to keep it last in the array
   ],
   // for avoiding CORS while developing Netlify Functions locally
