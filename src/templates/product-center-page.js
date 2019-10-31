c9d87d136d273844fd65909a2ba5a5e0bff2df32
Dipper GUO <tiglapiles@gmail.com>
2019-10-31 20:03:27 +0800

    add product table ...
    1. add product table component
    2. tabs style and react
    3. datapool style

--- src/templates/product-center-page.js
+++ src/templates/product-center-page.js
@@ -2,9 +2,10 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import Layout from "../components/Layout";
-import PreviewCompatibleImage from "../components/PreviewCompatibleImage"
+import PreviewCompatibleImage from "../components/PreviewCompatibleImage";
 import ProductTitle from "../components/ProductTitle";
 import ProductMethodTitle from "../components/ProductMethodTitle";
+import ProductAdvantageTable from "../components/ProductAdvantageTable";
 
 const SeekerTabContent = ({ method, advantage, imageInfo = {} }) => {
   const { image } = imageInfo;
@@ -19,13 +20,11 @@ const SeekerTabContent = ({ method, advantage, imageInfo = {} }) => {
           </div>
         </div>
       </section>
-      <section className="section section--gradient">
+      <section className="section h-section section--gradient">
         <div className="container">
           <div className="columns">
             <div className="column is-10 is-offset-1">
-              <div className="content">
-                <div>{advantage.heading}</div>
-              </div>
+              <ProductAdvantageTable tableInfo={advantage}/>
             </div>
           </div>
         </div>
@@ -37,7 +36,7 @@ const SeekerTabContent = ({ method, advantage, imageInfo = {} }) => {
             backgroundImage: `url(${
               !!image ? image.childImageSharp.fluid.src : image
             })`,
-            backgroundSize: "100% 100%",
+            backgroundSize: "cover",
             backgroundPosition: "center"
           }}
         ></div>
@@ -50,7 +49,7 @@ const InfoPlusTabContent = ({ method, advantage, imageInfo }) => {
   const { image } = imageInfo;
   return (
     <div>
-      <section className="section has-background-grey-lighter">
+      <section className="section h-section has-background-grey-1">
         <div className="container">
           <div className="columns">
             <div className="column is-10 is-offset-1">
@@ -59,13 +58,11 @@ const InfoPlusTabContent = ({ method, advantage, imageInfo }) => {
           </div>
         </div>
       </section>
-      <section className="section section--gradient">
+      <section className="section h-section section--gradient">
         <div className="container">
           <div className="columns">
             <div className="column is-10 is-offset-1">
-              <div className="content">
-                <div>{advantage.heading}</div>
-              </div>
+              <ProductAdvantageTable tableInfo={advantage}/>
             </div>
           </div>
         </div>
@@ -74,12 +71,11 @@ const InfoPlusTabContent = ({ method, advantage, imageInfo }) => {
         <div className="container">
           <div className="columns">
             <div className="column is-10 is-offset-1">
-              <div className="is-size-2-5 has-text-centered">方案架构</div>
-    <PreviewCompatibleImage imageInfo={{...imageInfo, style:{borderRadius:"unset"}}}/>
+              <div className="is-size-2-5 has-text-centered has-margin-bottom-40">方案架构</div>
+                <PreviewCompatibleImage imageInfo={{...imageInfo, style:{borderRadius:"unset"}}}/>
             </div>
           </div>
         </div>
-
       </section>
     </div>
   );
@@ -87,7 +83,7 @@ const InfoPlusTabContent = ({ method, advantage, imageInfo }) => {
 
 const HDPPTabContent = ({ method, datapool }) => (
   <div>
-    <section className="section has-background-grey-lighter">
+    <section className="section h-section has-background-grey-1">
       <div className="container">
         <div className="columns">
           <div className="column is-10 is-offset-1">
@@ -100,15 +96,15 @@ const HDPPTabContent = ({ method, datapool }) => (
       <div className="container">
         <div className="columns">
           <div className="column is-10 is-offset-1">
-            <div className="is-size-2-5 has-text-centered">数据池</div>
+            <div className="is-size-2-5 has-text-centered has-margin-bottom-40">数据池</div>
             <div className="columns is-multiline">
               {datapool.map((o,i) => (
                 <div className="column is-4" key={i}>
-                  <figure className="image is-48x48">
+                  <figure className="image is-48x48 margin-auto">
                     <PreviewCompatibleImage imageInfo={o}/>
                   </figure>
-                  <div className="is-size-6 has-text-centered">{o.head}</div>
-                  <div className="is-size-6-5">{o.content}</div>
+                  <div className="is-size-6 has-text-centered has-text-333" style={{lineHeight:'21px',padding:'20px'}}>{o.head}</div>
+                  <div className="is-size-6-5 has-text-666" style={{lineHeight:'26px'}}>{o.content}</div>
                 </div>
               ))}
             </div>
@@ -122,23 +118,34 @@ const HDPPTabContent = ({ method, datapool }) => (
 class ProductCenterTemplate extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      activeTab: {},
+      tabs: this.props.tabs.map(i => i.index==='seeker' ? {...i, className: 'is-active'} : i)
+    };
   }
 
   componentDidMount() {
-    console.log(this);
+    this.setState({ activeTab: { seeker: 'is-active' }});
+  }
+
+  handleTab = (index) => {
+    const { tabs } = this.state;
+    this.setState({
+      activeTab:{ [index]:'is-active' },
+      tabs: tabs.map(i => i.index===index ? {...i, className: 'is-active'} : { ...i, className: ''})
+    });
   }
 
   render() {
     const {
       image,
       header,
-      tabs,
       methods,
       advantages,
       banner,
       datapool
     } = this.props;
+    const { activeTab, tabs } = this.state;
     return (
       <div className="product-center">
         <div
@@ -163,11 +170,11 @@ class ProductCenterTemplate extends Component {
             <div className="columns">
               <div className="column is-10 is-offset-1">
                 <div className="tabs-header">
-                  <div className="tabs">
+                  <div className="tabs is-left">
                     <ul>
                       {tabs.map((o, i) => (
-                        <li className={``} key={i}>
-                          <a href={`#${o.index}`}>{o.name}</a>
+                        <li key={i} onClick={() => this.handleTab(o.index)} className={o.className}>
+                          <a>{o.name}</a>
                         </li>
                       ))}
                     </ul>
@@ -179,21 +186,21 @@ class ProductCenterTemplate extends Component {
         </section>
         {/* tabs content */}
         <div className="tabs-content">
-          <div className="tab-content ">
+          <div className={`tab-content ${activeTab.seeker}`}>
             <SeekerTabContent
               method={methods[0]}
               advantage={advantages[0]}
               imageInfo={banner[0]}
             />
           </div>
-          <div className="tab-content">
+          <div className={`tab-content ${activeTab.infoplus}`}>
             <InfoPlusTabContent
               method={methods[1]}
               advantage={advantages[1]}
               imageInfo={banner[1]}
             />
           </div>
-          <div className="tab-content is-active">
+          <div className={`tab-content ${activeTab.hdpp}`}>
             <HDPPTabContent method={methods[2]} datapool={datapool} />
           </div>
         </div>
