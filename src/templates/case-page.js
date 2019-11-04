51492ba9bc7a2d975455f676934aa452bde38bc6
Dipper GUO <tiglapiles@gmail.com>
2019-11-04 16:43:02 +0800

    case page mobile
    1. mobile style
    2. banner switch component
    3. mobile images
    4. page content modified
    5. ui:
    https://gitlab.com/tiglapiles/gatsby-starter-netlify-cms/tree/UI-design-sketch/ui/2019-11-4
    http://120.24.171.174:10080/haetek/company-official-website/tree/UI-design-sketch/ui

--- src/templates/case-page.js
+++ src/templates/case-page.js
@@ -1,32 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { graphql } from "gatsby";
+import {graphql} from "gatsby";
 import Layout from "../components/Layout";
+import BackgroundImageSwitch from "../components/BackgroundImageSwitch";
 import CaseCard from "../components/CaseCard";
 
-const CaseTemplate = ({ image, heading, cases }) => {
+const CaseTemplate = ({images, heading, cases}) => {
   return (
     <div className="case-page">
-      <div
-        className="full-width-image margin-top-0"
-        style={{
-          height: "600px",
-          backgroundImage: `url(${
-            !!image ? image.childImageSharp.fluid.src : image
-          })`,
-          backgroundSize: "cover",
-          backgroundPosition: "center"
-        }}
+      <BackgroundImageSwitch
+        images={images}
+        switchHeight={["600px", "222px"]}
       />
       <div className="section h-section">
         <div className="container">
-          <h3 className="is-size-3 is-size-5-mobile has-text-centered has-margin-bottom-40">
+          <h3 className="is-size-3 is-size-5-mobile has-text-centered has-margin-bottom-40 has-margin-bottom-15-mobile">
             {heading}
           </h3>
           <div className="case-container">
             <div className="columns is-multiline">
               {cases.map((o, i) => (
-                <div className="column is-4">
+                <div className="column is-4" key={i}>
                   <CaseCard info={o} key={i} />
                 </div>
               ))}
@@ -39,24 +33,24 @@ const CaseTemplate = ({ image, heading, cases }) => {
 };
 
 CaseTemplate.propTypes = {
-  image: PropTypes.object,
+  images: PropTypes.array,
   heading: PropTypes.string,
   cases: PropTypes.arrayOf(
     PropTypes.shape({
       image: PropTypes.object,
       alt: PropTypes.string,
       head: PropTypes.string,
-      content: PropTypes.string
-    })
-  )
+      content: PropTypes.string,
+    }),
+  ),
 };
 
-const Case = ({ data }) => {
-  const { frontmatter } = data.markdownRemark;
+const Case = ({data}) => {
+  const {frontmatter} = data.markdownRemark;
   return (
     <Layout>
       <CaseTemplate
-        image={frontmatter.image}
+        images={frontmatter.images}
         heading={frontmatter.heading}
         cases={frontmatter.cases}
       />
@@ -65,25 +59,37 @@ const Case = ({ data }) => {
 };
 
 Case.propTypes = {
-  data: PropTypes.object
+  data: PropTypes.shape({
+    frontmatter: PropTypes.object,
+  }),
 };
 
 export default Case;
 
 export const casesQuery = graphql`
   query Case($id: String!) {
-    markdownRemark(id: { eq: $id }) {
+    markdownRemark(id: {eq: $id}) {
       frontmatter {
-        image {
-          childImageSharp {
-            fluid(maxWidth: 2048, quality: 100) {
-              ...GatsbyImageSharpFluid
+        images {
+          image {
+            childImageSharp {
+              fluid(maxWidth: 2048, quality: 100) {
+                ...GatsbyImageSharpFluid
+              }
             }
           }
+          alt
         }
         heading
         cases {
-          image {
+          image1 {
+            childImageSharp {
+              fluid(maxWidth: 600, quality: 100) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          image2 {
             childImageSharp {
               fluid(maxWidth: 600, quality: 100) {
                 ...GatsbyImageSharpFluid
