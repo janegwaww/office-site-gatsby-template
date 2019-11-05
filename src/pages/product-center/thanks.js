9f6acf1ddd566279cf7833d6777f54c45bcad3b0
Dipper GUO <tiglapiles@gmail.com>
2019-11-05 16:11:38 +0800

    add email sender
    1. sender emailjs
    2. form modified

---
+++ src/pages/product-center/thanks.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../../components/Layout";
+
+export default () => (
+  <Layout>
+    <section className="section">
+      <div className="container">
+        <div className="content">
+          <h1>Thank you!</h1>
+          <p>我们将尽快回复您的反馈！</p>
+        </div>
+      </div>
+    </section>
+  </Layout>
+);
