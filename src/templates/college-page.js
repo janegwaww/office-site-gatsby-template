365c28af025de6ed62dbc1073db11973470be19e
Dipper GUO <tiglapiles@gmail.com>
2019-11-08 18:00:16 +0800

    update: college background  instruction
    text paragragh base on platform

--- src/templates/college-page.js
+++ src/templates/college-page.js
@@ -1,25 +1,44 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import PropTypes from "prop-types";
 import {graphql} from "gatsby";
 import Layout from "../components/Layout";
 import StudyDirection from "../components/StudyDirection";
 import BackgroundImageSwitch from "../components/BackgroundImageSwitch";
 
-const BackgroundInfo = ({heading = "", bakPara = []}) => (
-  <div className="college-back-info columns is-centered">
-    <div className="column is-11 has-text-centered">
-      <h3 className="college-back-info-head is-size-3 is-size-5-mobile">
-        {heading}
-      </h3>
-      <br />
-      <div className="is-size-6 is-size-7-mobile has-text-left-mobile">
-        {bakPara.map((o, i) => (
-          <p key={i}>{o}</p>
-        ))}
+const BackgroundInfo = ({heading = "", bakPara = []}) => {
+  let [para, setPara] = useState(<></>);
+  useEffect(() => {
+    if (window.innerWidth <= 768) {
+      setPara(
+        <>
+          <p>{`${bakPara[0]}${bakPara[1]}`}</p>
+          <p>{`${bakPara[2]}${bakPara[3]}`}</p>
+        </>,
+      );
+    } else {
+      setPara(
+        <>
+          {bakPara.map((o, i) => (
+            <p key={i}>{o}</p>
+          ))}
+        </>,
+      );
+    }
+  }, []);
+  return (
+    <div className="college-back-info columns is-centered">
+      <div className="column is-11 has-text-centered">
+        <h3 className="college-back-info-head is-size-3 is-size-5-mobile">
+          {heading}
+        </h3>
+        <br />
+        <div className="is-size-6 is-size-7-mobile has-text-left-mobile">
+          {para}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 const CollegeTemplate = ({images, background, direction}) => {
   const bakPara = background.description.split(/\s{2}|\\/);
