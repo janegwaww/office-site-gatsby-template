91fdae8e30af8af896fa832192c26c2300e946c7
dipper <18759315970@163.com>
2021-01-12 10:28:06 +0800

    feat: 发展历程组件部份

--- src/components/AboutUs/History.jsx
+++ src/components/AboutUs/History.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import SectionTitle from "../SectionTitle";
 
 const History = () => {
-  return <div className="history">history</div>;
+  return (
+    <div className="history">
+      <SectionTitle title="发展历程" subtitle="DEVELOPMENT HISTORY" />
+    </div>
+  );
 };
 
 export default History;
