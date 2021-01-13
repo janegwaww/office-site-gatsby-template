51c2e176a3d3b8cf58a382d8c8fdd837f8ae0f0e
dipper <18759315970@163.com>
2021-01-13 09:27:03 +0800

    fix: init coretech page

--- src/templates/coretech-page.jsx
+++ src/templates/coretech-page.jsx
@@ -2,10 +2,36 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import Layout from "../components/Layout";
+import BackgroundImageSwitch from "../components/BackgroundImageSwitch";
+import DoublePanelLayer from "../components/DoublePanelLayer";
+import SectionTitle from "../components/SectionTitle";
 
 class CoretechTemplate extends Component {
   render() {
-    return <div style={{ height: "100vh" }}>coretech template</div>;
+    return (
+      <div className="core-tech">
+        <BackgroundImageSwitch
+          switchHeight={["570px", "190px"]}
+          images={[
+            { image: "/img/index-banner.png" },
+            { image: "/img/index-banner-mobile.png" }
+          ]}
+        />
+        <div style={{ height: "300px" }}>
+          <SectionTitle
+            title="视频封面合成"
+            subtitle="Video cover composition"
+          />
+        </div>
+        <DoublePanelLayer title="应用特色" subtitle="Application features" />
+        <div style={{ height: "600px" }}>
+          <SectionTitle title="应用场景" subtitle="Application scenarios" />
+        </div>
+        <div style={{ height: "300px" }}>
+          <SectionTitle title="合作案例" subtitle="Cooperation cases" />
+        </div>
+      </div>
+    );
   }
 }
 
